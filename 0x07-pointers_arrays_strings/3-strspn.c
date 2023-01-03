9681ae33eac74320e0843c19474e72fe6440c890
Nasaukwuoma <ukwuomaursulla@gmail.com>
2023-01-02 19:04:09 -0800

    0x07 project

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,11 +13,11 @@ unsigned int _strspn(char *s, char *accept)
 		int i, j, f;
 
 			i = 0;
-			while (s[i] != '\0')
+		       while (s[i] != '\0')
 {
 	j = 0;
 
-	f = 1; / flag status /													while (accept[j] != '\0')											
+	f = 1; /flag status/													while (accept[j] != '\0')											
 {
 	if (s[i] == accept[j])
 
@@ -25,13 +25,11 @@ unsigned int _strspn(char *s, char *accept)
 	f = 0; / success /
 		break;
 }
-																	j++;
-
-																	}
-          if (f == 1)
+      j++;															}
+      if (f == 1)
 																	break;
 	i++;
 																}
 
-			return (i);
+return (i);
 }
