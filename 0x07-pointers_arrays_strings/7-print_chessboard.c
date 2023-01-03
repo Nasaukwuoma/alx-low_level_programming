a339a96f6c703d2055038bc8cd4a62f519368b51
Nasaukwuoma <ukwuomaursulla@gmail.com>
2023-01-02 19:47:57 -0800

    arrays

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- *  * print_chessboard - print the chessboard, 2d array
- *   * @a: 2d array of chars
- *    * Return: void
- **/
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ * Return: void
+ */
 void print_chessboard(char (*a)[8])
 {
-		/variables/
-		int n, j;
-		n = 0;
+	/*variables*/
+	int n, j;
 
-		while (n < 8) /repeat number/
+
+
+	n = 0;
+
+	while (n < 8) /*repeat number*/
 
 	{
 
@@ -19,12 +22,15 @@ void print_chessboard(char (*a)[8])
 
 		while (j < 8)
 
-	{
-																_putchar (a[n][j]);
-																	j++;
-																	}
+		{
 
-	_putchar ('\n'); /print new line/
-																n++;
+			_putchar (a[n][j]);
+
+			j++;
+
+		}
+
+		_putchar ('\n'); /*print new line*/
+		n++;
 	}
 }
